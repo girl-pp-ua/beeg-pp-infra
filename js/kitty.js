2c85e3991c9d3c8ec1c900eba27f879cd25dbaa4
griffi-gh <prasol258@gmail.com>
2024-09-25 01:29:59 +0200

    add kitty :3

---
+++ js/kitty.js
@@ -0,0 +1,12 @@
+// @license magnet:?xt=urn:btih:d3d9a9a6595521f9666a5e94cc830dab83b65699&dn=expat.txt MIT
+
+const ONEKO = "https://cdn.jsdelivr.net/gh/adryd325/oneko.js@b323d71126726e4487c10f0d75fc47ac37ead11c";
+
+fetch(`${ONEKO}/oneko.js`)
+    .then(response => response.text())
+    .then(text => (new Function(
+        // Fix image URL
+        text.replace("./oneko.gif", `${ONEKO}/oneko.gif`)
+    ))());
+
+// @license-end
